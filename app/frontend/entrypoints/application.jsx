ef263e5e425a2f922d7159828d740939685f9fd5
yudai142 <54573782+yudai142@users.noreply.github.com>
2026-09-05 16:57:13 +0900

    Inertia.js ページ解決: 明示的コンポーネント登録に修正
    - RoomsIndex, RoomsShow ページを entrypoint に登録
    - pageMap オブジェクトで全ページを管理
    - console ログで解決プロセスを記録
    - エラーハンドリング強化

--- app/frontend/entrypoints/application.jsx
+++ app/frontend/entrypoints/application.jsx
@@ -3,21 +3,28 @@ import { createInertiaApp } from '@inertiajs/react'
 import { createRoot } from 'react-dom/client'
 import Layout from '../components/Layout'
 
-// Import pages directly
 import Home from '../Pages/Home'
+import RoomsIndex from '../Pages/Rooms/Index'
 import RoomsNew from '../Pages/Rooms/New'
+import RoomsShow from '../Pages/Rooms/Show'
 import RoomsCanvasEditor from '../Pages/Rooms/CanvasEditor'
 
-const pages = {
-  'Home': { default: Home },
-  'Rooms/New': { default: RoomsNew },
-  'Rooms/CanvasEditor': { default: RoomsCanvasEditor },
+const pageMap = {
+  'Home': Home,
+  'Rooms/Index': RoomsIndex,
+  'Rooms/New': RoomsNew,
+  'Rooms/Show': RoomsShow,
+  'Rooms/CanvasEditor': RoomsCanvasEditor,
 }
 
 createInertiaApp({
   resolve: name => {
-    console.log('Inertia resolve:', { name, available: Object.keys(pages) })
-    return pages[name]?.default
+    console.log('Resolving page:', name, 'Available:', Object.keys(pageMap))
+    const component = pageMap[name]
+    if (!component) {
+      throw new Error(`Page not found: ${name}`)
+    }
+    return component
   },
   setup({ el, App, props }) {
     const root = createRoot(el)
