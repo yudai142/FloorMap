be1cae7f946fcf860b9e217eb48a507beef91bb5
yudai142 <54573782+yudai142@users.noreply.github.com>
2026-09-05 19:43:14 +0900

    トップページアクセス時、ログイン状態ならrooms#indexへリダイレクト

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
+    return redirect_to rooms_path if user_signed_in?
     render inertia: "Home"
   end
 end
