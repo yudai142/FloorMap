42ffb3fba9ac9d06313bde456e8d07649e27a636
yudai142 <54573782+yudai142@users.noreply.github.com>
2026-09-06 15:17:02 +0900

    Seat ファクトリーのシーケンス範囲を拡大
    position_x/position_y の一意性制約に対応し、
    より広い範囲で異なる値を生成するよう改善

--- spec/factories/seats.rb
+++ spec/factories/seats.rb
@@ -1,10 +1,14 @@
 FactoryBot.define do
   factory :seat do
+    transient do
+      seat_index { 0 }
+    end
+
     room { association :room }
     sequence(:row_number) { |n| n % 10 }
     sequence(:column_number) { |n| (n % 8) + 1 }
-    sequence(:position_x) { |n| ((n % 50) + 1) * 30 }
-    sequence(:position_y) { |n| ((n / 50) + 1) * 30 }
+    sequence(:position_x) { |n| ((n % 100) + 1) * 20 }
+    sequence(:position_y) { |n| ((n / 100) + 1) * 20 }
     seat_type { :regular }
 
     trait :accessible do
