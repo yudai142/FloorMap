9dc5f25d40d62022f4528560096a88dd360f6323
yudai142 <54573782+yudai142@users.noreply.github.com>
2026-09-06 15:11:31 +0900

    Seat ファクトリーに position_x/position_y を追加
    テスト実行時の Seat 生成で position_x と position_y が必須なため、
    sequence で自動生成するように修正

--- spec/factories/seats.rb
+++ spec/factories/seats.rb
@@ -3,6 +3,8 @@ FactoryBot.define do
     room { association :room }
     sequence(:row_number) { |n| n % 10 }
     sequence(:column_number) { |n| (n % 8) + 1 }
+    sequence(:position_x) { |n| (n % 10) * 100 + 50 }
+    sequence(:position_y) { |n| (n / 10) * 100 + 50 }
     seat_type { :regular }
 
     trait :accessible do
