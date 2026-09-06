67ed4ce3bb2fdce2085a1d130b1e4050eb1c3b19
yudai142 <54573782+yudai142@users.noreply.github.com>
2026-09-06 13:20:15 +0900

    Show.jsx：useRef を import に追加

--- app/frontend/Pages/Rooms/Show.jsx
+++ app/frontend/Pages/Rooms/Show.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { usePage } from '@inertiajs/react'
 
 export default function RoomShow() {
