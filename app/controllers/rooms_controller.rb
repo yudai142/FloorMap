611b44d79b5fa7f35fd4b7b3e68a753079de6650
yudai142 <54573782+yudai142@users.noreply.github.com>
2026-09-06 11:42:35 +0900

    非ログインユーザーが rooms#show ページにアクセス可能に
    - RoomsController の show と canvas_data から認証を除外
    - share_token を知っていれば誰でもルーム情報を閲覧可能

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,5 +1,6 @@
 class RoomsController < ApplicationController
   before_action :set_room, only: [ :show, :edit, :update, :destroy, :canvas_data, :canvas_editor, :floor_plan ]
+  skip_before_action :authenticate_user!, only: [ :show, :canvas_data ]
 
   def index
     authorize Room
