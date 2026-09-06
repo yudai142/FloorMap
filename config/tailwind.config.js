02cb1fb16d8f6402fbfe04ec25bed487bc02b799
yudai142 <54573782+yudai142@users.noreply.github.com>
2026-09-06 16:12:00 +0900

    fix: TailwindCSS v4 設定ファイルを追加
    TailwindCSS v4 は config/tailwind.config.js が必要です。
    input パスを app/assets/stylesheets/application.css に設定

---
+++ config/tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  content: [
+    './app/views/**/*.{html,erb,haml}',
+    './app/components/**/*.{html,erb,haml}',
+    './app/javascript/**/*.{js,jsx,ts,tsx}'
+  ],
+  input: './app/assets/stylesheets/application.css'
+}
