39b5ac77d8f57c035be2e10a427026910f24c48a
yudai142 <54573782+yudai142@users.noreply.github.com>
2026-09-06 17:20:15 +0900

    Inertia.jsのSSRを無効化（本番環境対応）

--- config/initializers/inertia.rb
+++ config/initializers/inertia.rb
@@ -1,4 +1,4 @@
 InertiaRails.configure do |config|
   config.version = -> { File.mtime("#{Rails.root}/app/frontend").to_i }
-  config.ssr_enabled = !Rails.env.development?
+  config.ssr_enabled = false
 end
