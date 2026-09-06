92823e2859b61c6924e30291d6eee8102821234c
yudai142 <54573782+yudai142@users.noreply.github.com>
2026-09-06 09:36:12 +0900

    RoomPermissions コントローラーの set_room メソッドを修正
    nested route のパラメータ名が :share_token の場合にも対応
    params[:share_token] || params[:room_share_token] で両方をチェック

--- app/controllers/room_permissions_controller.rb
+++ app/controllers/room_permissions_controller.rb
@@ -2,7 +2,8 @@ class RoomPermissionsController < ApplicationController
   before_action :set_room_permission, only: :destroy
 
   def create
-    @room = Room.find_by(share_token: params[:room_share_token])
+    token = params[:share_token] || params[:room_share_token]
+    @room = Room.find_by(share_token: token)
     raise ActiveRecord::RecordNotFound if @room.blank?
     @room_permission = @room.room_permissions.build(room_permission_params)
     authorize @room_permission, :create?
