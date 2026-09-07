f6f5feb487275e67fe9aa69b78847d7e421ebc7c
yudai142 <54573782+yudai142@users.noreply.github.com>
2026-09-07 20:38:28 +0900

    Devise エラーメッセージを日本語化
    - config/application.rb にデフォルトロケール設定を追加
    - config/locales/devise.ja.yml を作成
    - Devise のエラーメッセージが日本語で表示されるように設定

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,10 @@ module FloorMap
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
+    # Set default locale to Japanese
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
+
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
   end
